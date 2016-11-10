bb5188c441c63a4d42ba899268375a6e9d6af49a
Jake Thurman <jacob@thurmans.com>
2016-11-09 22:07:04 -0500

    Remove left-over print statement from debugging

--- fonts.py
+++ fonts.py
@@ -10,7 +10,6 @@ def random_font(size=LINK_TEXT_SIZE):
 
 def random_font_factory():
 	my_font = random.choice(("OpenSans-Regular", "OpenSans-Regular", "OpenSans-Regular", "mypager", "stocky"))
-	print(my_font)
 	
 	def font_factory(size=LINK_TEXT_SIZE):
 		return font.Font("fonts\\" + my_font + ".ttf", size)
